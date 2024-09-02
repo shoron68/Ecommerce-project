b278b8243887ca9555b2a183f9067b79357fdcc9
shoron68 <mr.shoron.7514@gmail.com>
2024-09-03 01:59:11 +0600

    demo

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -63,7 +63,7 @@ const Menu = () => {
 
     return (
         <Container>
-            <div className="lg:flex lg:justify-between items-center py-4 px-4 lg:px-0 relative ">
+            <div className="lg:flex lg:justify-between items-center py-4 px-4 lg:px-0  ">
                 <div className="lg:w-[70%] w-full flex justify-between items-center ">
                     <div className="w-[10%]">
                         <Link to="/"><h2 className='text-[24px] lg:text-[39px] text-[#0D0E43] font-josefin font-bold'>Hekto</h2></Link>
